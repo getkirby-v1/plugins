21dbe75f29f34b84b1396f935ecce5cb6c5816ee
Bastian Allgeier <bastian.allgeier@gmail.com>
2011-12-15 12:33:48 +0100

    new version of the feed snippet (1.1)

--- snippets/feed/feed.php
+++ snippets/feed/feed.php
@@ -1,5 +1,15 @@
-<?php header('Content-type: text/xml; charset="utf-8"') ?>
-<?php echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>" ?>
+<?php 
+
+// defaults
+if(!isset($descriptionExcerpt)) $descriptionExcerpt = true; 
+
+// send the right header
+header('Content-type: text/xml; charset="utf-8"');
+
+// echo the doctype
+echo '<?xml version="1.0" encoding="utf-8"?>';
+
+?>
 <!-- generator="<?php echo c::get('feed.generator', 'Kirby') ?>" -->
 
 <rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:wfw="http://wellformedweb.org/CommentAPI/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:atom="http://www.w3.org/2005/Atom">
@@ -23,7 +33,11 @@
       <pubDate><?php echo ($item->date()) ? date('r', $item->date()) : date('r', $item->modified()) ?></pubDate>
         
       <?php if(isset($descriptionField)): ?>
-      <description><?php echo xml(excerpt($item->{$descriptionField}, (isset($descriptionLength)) ? $descriptionLength : 140 )) ?></description>
+      <?php if(!$descriptionExcerpt): ?>
+      <description><![CDATA[<?php echo kirbytext($item->{$descriptionField}) ?>]]></description>      
+      <?php else: ?>
+      <description><![CDATA[<?php echo excerpt($item->{$descriptionField}, (isset($descriptionLength)) ? $descriptionLength : 140) ?>]]></description>
+      <?php endif ?>
       <?php endif ?>
 
     </item>
