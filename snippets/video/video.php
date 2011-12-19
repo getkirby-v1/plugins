e1895834efe422ce736ac60e84ca033721d2cc10
Bastian Allgeier <bastian.allgeier@gmail.com>
2011-12-19 17:54:07 +0100

    video snippet 1.0

---
+++ snippets/video/video.php
@@ -0,0 +1,24 @@
+<?php
+
+// stop without videos
+if(empty($videos)) return;
+
+// set some defaults
+if(!isset($width))    $width    = 400;
+if(!isset($height))   $height   = 300;
+if(!isset($preload))  $preload  = true;
+if(!isset($controls)) $controls = true;
+
+// build the html tags for the video element
+$preload  = ($preload)  ? ' preload="preload"'   : '';
+$controls = ($controls) ? ' controls="controls"' : '';
+
+?>
+<video width="<?php echo $width ?>" height="<?php echo $height ?>"<?php echo $preload . $controls ?>>
+  <?php foreach($videos as $video): ?>
+  <source src="<?php echo $video->url() ?>" type="<?php echo $video->mime() ?>" />
+  <?php endforeach ?>
+  <?php if(isset($thumb)): ?>
+  <img src="<?php echo $thumb->url() ?>" alt="<?php echo $thumb->title() ?>" />
+  <?php endif ?>
+</video>
