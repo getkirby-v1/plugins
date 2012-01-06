e63b460a21dfd20b260d1105cea5c5e28b1cd5f4
Bastian Allgeier <bastian.allgeier@gmail.com>
2012-01-06 14:10:30 +0100

    fixed a bug in the feed.php template

--- templates/feed/feed.php
+++ templates/feed/feed.php
@@ -3,7 +3,7 @@
 // get any list of items    
 // in this case we get all visible children of the blog section, 
 // flip them to get them in reverse order and make sure we only get the last 10
-$items = $pages->find('blog')->children()->visible()->flip()->limit(0,10);
+$items = $pages->find('blog')->children()->visible()->flip()->limit(10);
 
 // this is how you embed the feed snippet with some options    
 snippet('feed', array(
