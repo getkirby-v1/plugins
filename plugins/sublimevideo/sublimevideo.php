d39ba3145c3533cad73b162030179e3d00f36b74
Bastian Allgeier <bastian.allgeier@gmail.com>
2012-03-06 10:39:48 +0100

    new version of the sublime video

--- plugins/sublimevideo/sublimevideo.php
+++ plugins/sublimevideo/sublimevideo.php
@@ -1,48 +1,85 @@
 <?php 
 
-	// Version: 1
-	// Date: 14.02.2012
-	// Author: Thibaut Ninove <http://wooconcept.com>, with the help of Bastian Allgeier
-	// Support: provided as is, no support provided
+// Version: 1.1
+// Date: 06.03.2012
+// Author: Thibaut Ninove <http://wooconcept.com>, with the help of Bastian Allgeier <http://getkirby.com>
+// Support: provided as is, no support provided
 
-	class kirbytextExtended extends kirbytext {
-	
-		function __construct($text, $markdown=true) {
-		
-			parent::__construct($text, $markdown);
-			
-			$this->addTags('sublime');
-			$this->addAttributes('poster', 'video1', 'video2');
-	
-		}
-	
-		function sublime($params) {
-		
-			global $site;
-			
-			$page = $site->pages()->active();                
-			$file = url($page->diruri() . '/' . $params['sublime']);
-			
-			$defaults = array(
-				'poster' => 'jpg',
-				'video1' => 'mp4',
-				'video2' => 'webm',
-				'width' => '',
-				'height' => ''
-			);
-			
-			$options = array_merge($defaults, $params);
-			
-			$poster = $file . '.' . html($options['poster']);
-			$video1 = $file . '.' . html($options['video1']);
-			$video2 = $file . '.' . html($options['video2']);
-			$width  = html($options['width']);
-			$height = html($options['height']);
-			
-			return '<video class="sublime" poster="' . $poster . '" width="' . $width . '" height="' . $height . '"><source src="' . $video1 . '"><source src="' . $video2 . '"></video>';
-			
-		}
-	
-	}
+class kirbytextExtended extends kirbytext {
+
+  function __construct($text, $markdown=true) {
+  
+    parent::__construct($text, $markdown);
+    
+    $this->addTags('sublime');
+    $this->addAttributes('uid', 'name');
+
+  }
+
+  function sublime($params) {
+  
+    global $site;
+    
+    $page   = $site->pages()->active();                
+    $id     = @$params['sublime'];
+    $class  = @$params['class'];
+    $videos = array();
+    $poster = false;
+    
+    // gather all video files which match the given id/name    
+    foreach($page->videos() as $v) {
+
+      if(preg_match('!^' . preg_quote($id) . '!i', $v->name())) {
+        
+        $extension = f::extension($v->name());
+        $mobile    = ($extension == 'mobile') ? $v->mobile = true : $v->mobile = false;
+        $hd        = ($extension == 'hd')     ? $v->hd     = true : $v->hd     = false;
+                      
+        $videos[] = $v;
+
+      }
+
+    }
+    
+    if(empty($videos)) return false;    
+
+    // find the poster for this video
+    foreach($page->images() as $i) {
+      if(preg_match('!^' . preg_quote($id) . '!i', $i->name())) {
+        $poster = $i;
+        break;
+      }
+    }
+        
+    $defaults = array(
+      'uid'    => $id,
+      'name'   => $id,
+      'width'  => c::get('kirbytext.video.width'),
+      'height' => c::get('kirbytext.video.height')
+    );
+    
+    $options = array_merge($defaults, $params);
+    $width   = html($options['width']);
+    $height  = html($options['height']);
+    $uid     = html($options['uid']);
+    $name    = html($options['name']);
+    
+    // create an additional css class if specified
+    if(!empty($class)) $class = ' ' . html($class);
+    
+    $html = '<video class="sublime' . $class . '" poster="' . $poster->url() . '" width="' . $width . '" height="' . $height . '" data-uid="' . $uid . '" data-name="' . $name . '" preload="none">'; 
+    foreach($videos as $video) {
+      // check for hd quality 
+      $hd = ($video->hd()) ? ' data-quality="hd"' : '';
+      // generate the source tag for each video
+      $html .= '<source src="' . $video->url() . '"' . $hd . '>';
+    }
+    $html .= '</video>';
+        
+    return $html;
+    
+  }
+
+}
 
 ?>
