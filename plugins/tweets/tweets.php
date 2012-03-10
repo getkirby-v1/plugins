320f50bc70492f2aa69975cb31d9b4f2524ce480
Bastian Allgeier <bastian.allgeier@gmail.com>
2012-03-10 20:27:20 +0100

    New Tweets plugin

---
+++ plugins/tweets/tweets.php
@@ -0,0 +1,83 @@
+<?php
+
+function tweets($username, $params=array()) {
+
+  $defaults = array(
+    'limit'   => 10,
+    'cache'   => true,
+    'refresh' => 60*20 // refresh every 20 minutes
+  );
+  
+  // add the username to the defaults array  
+  $defaults['username'] = $username;
+
+  $options = array_merge($defaults, $params);
+
+  // check the cache dir 
+  $cacheDir = c::get('root.cache') . '/tweets';
+  dir::make($cacheDir);
+
+  // disable the cache if adding the cache dir failed  
+  if(!is_dir($cacheDir) || !is_writable($cacheDir)) $options['cache'] = false;
+    
+  // sanitize the limit
+  if($options['limit'] > 200) $options['limit'] = 200;
+  
+  // generate a unique cache ID    
+  $cacheID = 'tweets/tweets.' . md5($options['username']) . '.' . $options['limit'] . '.php';
+  
+  if($options['cache']) {
+    $cache = (cache::modified($cacheID) < time()-$options['refresh']) ? false : cache::get($cacheID);  
+  } else {
+    $cache = false;
+  }
+  
+  if(!empty($cache)) return $cache;
+
+  $url  = 'http://api.twitter.com/1/statuses/user_timeline.json?screen_name=' . $options['username'] . '&count=' . $options['limit'];
+  $json = @file_get_contents($url);
+  $data = str::parse($json);  
+
+  if(!$data) return false;
+
+  $result = array();
+   
+  foreach($data as $tweet) {
+    
+    $user = $tweet['user'];
+                                            
+    $result[] = new tweet(array(
+      'url'    => 'http://twitter.com/' . $options['username'] . '/status/' . $tweet['id_str'],
+      'text'   => $tweet['text'],
+      'date'   => strtotime($tweet['created_at']),
+      'source' => $tweet['source'],
+      'user'   => new obj(array(
+        'name'      => $user['name'],
+        'bio'       => $user['description'], 
+        'username'  => $user['screen_name'],
+        'url'       => 'http://twitter.com/' . $user['screen_name'],
+        'image'     => 'http://twitter.com/api/users/profile_image/' . $user['screen_name'],
+        'following' => $user['friends_count'],
+        'followers' => $user['followers_count'],
+      ))
+    ));
+        
+  }
+    
+  $result = new obj($result);
+  
+  if($options['cache']) cache::set($cacheID, $result);
+    
+  return $result;
+  
+}
+
+class tweet extends obj {
+  
+  function date($format=false) {
+    return ($format) ? date($format, $this->date) : $this->date;  
+  }
+  
+}
+
+?>
