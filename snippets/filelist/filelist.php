2b65bbec534693d99b41ad6e8508806f28d132f1
Bart <bart@simplum.ch>
2012-05-10 13:18:38 +0300

    Removed filters on line 18 because the files are already filtered above that.. now the extension and sort params are really optional and not necessary anymore :)

--- snippets/filelist/filelist.php
+++ snippets/filelist/filelist.php
@@ -15,7 +15,7 @@ $files = $files->sortBy($sort, $direction);
 
 ?>
 <ul class="filelist">
-  <?php foreach($page->files()->findByExtension($extension)->sortBy($sort, $direction) as $file): ?>
+  <?php foreach($page->files() as $file): ?>
   <li><a href="<?php echo $file->url() ?>"><?php echo html($file->filename()) ?></a></li>    
   <?php endforeach ?>
 </ul>
