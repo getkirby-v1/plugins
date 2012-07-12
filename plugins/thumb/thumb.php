4e63e4743e8e6d825ca244a4e22df7d160552902
Bastian Allgeier <bastian.allgeier@gmail.com>
2012-07-12 14:53:25 +0200

    alpha channel support for pngs

--- plugins/thumb/thumb.php
+++ plugins/thumb/thumb.php
@@ -223,10 +223,16 @@ class thumb {
       
       // create a temporary resized version of the image first
       $thumb = imagecreatetruecolor($this->tmpWidth, $this->tmpHeight); 
+      imagesavealpha($thumb, true);
+      $color = imagecolorallocatealpha($thumb, 0, 0, 0, 127);
+      imagefill($thumb, 0, 0, $color);
       imagecopyresampled($thumb, $image, 0, 0, 0, 0, $this->tmpWidth, $this->tmpHeight, $this->sourceWidth, $this->sourceHeight); 
       
       // crop that image afterwards      
       $cropped = imagecreatetruecolor($this->width, $this->height); 
+      imagesavealpha($cropped, true);
+      $color   = imagecolorallocatealpha($cropped, 0, 0, 0, 127);
+      imagefill($cropped, 0, 0, $color);
       imagecopyresampled($cropped, $thumb, 0, 0, $startX, $startY, $this->tmpWidth, $this->tmpHeight, $this->tmpWidth, $this->tmpHeight); 
       imagedestroy($thumb);
       
@@ -235,6 +241,9 @@ class thumb {
 
     } else {
       $thumb = imagecreatetruecolor($this->width, $this->height); 
+      imagesavealpha($thumb, true);
+      $color = imagecolorallocatealpha($thumb, 0, 0, 0, 127);
+      imagefill($thumb, 0, 0, $color);
       imagecopyresampled($thumb, $image, 0, 0, 0, 0, $this->width, $this->height, $this->sourceWidth, $this->sourceHeight); 
     }    
     
@@ -252,5 +261,5 @@ class thumb {
     );
     
   }
-  
+    
 }
