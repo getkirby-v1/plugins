3c9d9d9ef8372c17dc7151218578dd22524f4b31
Bastian Allgeier <bastian.allgeier@gmail.com>
2012-10-30 17:29:24 +0100

    added more sample files to the auth plugin

---
+++ plugins/auth/accounts/demo.php
@@ -0,0 +1,10 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+# setup your account credentials here
+# it's highly recommended to use md5 or sha1 encryption 
+# for your passwords. read more about encryption in the
+# docs: http://getkirby.com/docs/panel/accounts
+
+username: demo
+password: demo
+group:    admin
