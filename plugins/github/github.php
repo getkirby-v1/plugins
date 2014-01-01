0f5be39c8529be8470401172d49ace5acbc035e0
Peter Persiel <peterpersiel@users.noreply.github.com>
2014-01-01 18:58:53 +0100

    GitHub API requires to set a user agent
    GitHub API requires to set a user agent -> set username as user agent

--- plugins/github/github.php
+++ plugins/github/github.php
@@ -70,6 +70,7 @@ class github {
 
         curl_setopt($handler, CURLOPT_URL, $url);
         curl_setopt($handler, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($handler, CURLOPT_USERAGENT, $this->username);
 
         $data = curl_exec($handler);
         curl_close($handler);
@@ -77,5 +78,4 @@ class github {
         return json_decode($data);
     }
 }
-
 ?>
