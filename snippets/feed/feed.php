ebc9e02e2b422330a9e2da88726afdd3f3def292
Nils Neumann <mail@der-zyklop.de>
2014-05-29 13:28:16 +0200

    compatibility with Kirby2

--- snippets/feed/feed.php
+++ snippets/feed/feed.php
@@ -34,9 +34,9 @@ echo '<?xml version="1.0" encoding="utf-8"?>';
 
       <?php if(isset($descriptionField)): ?>
       <?php if(!$descriptionExcerpt): ?>
-      <description><![CDATA[<?php echo kirbytext($item->{$descriptionField}) ?>]]></description>
+      <description><![CDATA[<?php echo kirbytext($item->{$descriptionField}()) ?>]]></description>
       <?php else: ?>
-      <description><![CDATA[<?php echo excerpt($item->{$descriptionField}, (isset($descriptionLength)) ? $descriptionLength : 140) ?>]]></description>
+      <description><![CDATA[<?php echo excerpt($item->{$descriptionField}(), (isset($descriptionLength)) ? $descriptionLength : 140) ?>]]></description>
       <?php endif ?>
       <?php endif ?>
 
